cc728a16da117aa5298ce8caf52606616f9bb2f8
erezt.dev <erezt.dev@gmail.com>
2018-03-15 14:25:38 +0200

    Init, godspeed.

---
+++ src/index.test.js
@@ -0,0 +1,10 @@
+import expect from 'expect';
+
+describe('Erez Test', () =>{
+   it('should pass', () =>{
+       expect(true).toEqual(true); // put my tests for react components here
+       //like this:
+       // var erez = {height: "1.87", eyes:"blue", specialMoves: 'karate-chop'};
+       // expect(erez.specialMoves).toEqual('karate-chop');
+   });
+});
